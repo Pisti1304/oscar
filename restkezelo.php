e3613f7d03abee7b63cd15605e4337baf4510a17
Pisti1304 <72573537332@szily.hu>
2025-03-10 12:18:46 +0100

    restkezelo

---
+++ restkezelo.php
@@ -0,0 +1,21 @@
+<?php
+
+class RestKezelo
+{
+    private $httpVersion =  "HTTP/1.1";
+    public function sethttpFejlec()
+    {
+        $statusUzenet = $this->gethttpStatusUzenet($statusKod)
+        header($this->httpVersion."".$statusKod
+        ."".$statusUzenet);
+        header("Content-Type")
+    }
+    public  function gethttpStatusUzenet($statusKod)
+    {
+        $httpstatus = array(
+            200  => 'OK',
+            400 =>'Bad Request',
+            404 =>'Not Found'
+        );
+    }
+}
